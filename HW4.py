943b7b6a3c6986239f98b30fc243c66f4aa81498
Amirah Aldajani <147912255+aldajani0@users.noreply.github.com>
2024-03-12 22:05:42 +0300

    Update HW4.py

--- HW4.py
+++ HW4.py
@@ -1,13 +1,25 @@
 def selection_sort(arr):
+    # Calculate the length of the array
     n = len(arr)
+    
+    # Iterate over each index in the array
     for i in range(n):
+        # Assume the current index is the minimum
         min_idx = i
+        
+        # Iterate over the array
         for j in range(i+1, n):
+            # Check if the current element is smaller than the minimum element
             if arr[j] < arr[min_idx]:
+                # Update the minimum index if a smaller element is found
                 min_idx = j
+        
+        # Swap the current element with the minimum element found
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
 
-
-arr = [12, 32, 12, 24, 10]
+# trying:
+arr = [76, 22, 14, 20, 9]
+# Sort the array using the function
 selection_sort(arr)
-print("my sorted array:", arr)
+# Print the sorted array
+print("Sorted array:", arr)
